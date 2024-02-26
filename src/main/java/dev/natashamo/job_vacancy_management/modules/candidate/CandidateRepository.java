933daff0cf64bfa25c99fd01c4db2c9311fee8f8
Natasha M Oliveira <me@natashamo.dev>
2024-02-25 22:46:12 -0300

    feat: create the persistence layer

---
+++ src/main/java/dev/natashamo/job_vacancy_management/modules/candidate/CandidateRepository.java
@@ -0,0 +1,9 @@
+package dev.natashamo.job_vacancy_management.modules.candidate;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+
+}
--- src/main/java/dev/natashamo/job_vacancy_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/dev/natashamo/job_vacancy_management/modules/candidate/controllers/CandidateController.java
@@ -1,18 +1,23 @@
 package dev.natashamo.job_vacancy_management.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.natashamo.job_vacancy_management.modules.candidate.CandidateEntity;
+import dev.natashamo.job_vacancy_management.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+  @Autowired
+  private CandidateRepository candidateRepository;
+
   @PostMapping("/")
-  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-    System.out.println(candidateEntity.getEmail());
+  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    return this.candidateRepository.save(candidateEntity);
   }
 }
